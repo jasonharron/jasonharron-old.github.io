f8d08fc5c70e4e45e9ebe6bd2bb699678c0a588f
Jason Harron <jasonharron@utexas.edu>
2019-12-12 17:10:14 -0600

    CutScene.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Level1v2 from './scenes/Level1.1.js';
 import IntroScene from './scenes/Introduction.js';
 import bossIntroScene from './scenes/bossIntroduction.js'
 import test from './scenes/test.js';
-import CutScene from './scenes/cutscene.js';
+import CutScene from './scenes/CutScene.js';
 import gameWorld from './scenes/gameWorld.js'
 
 
